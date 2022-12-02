50e6cfe91c4ca3c877a467c3a9efc00c0072d9c1
ber4bbit <ber4bbit@gmail.com>
2022-12-03 00:54:05 +0700

    added header and 3 sections

---
+++ js/burgerMenuHandler.js
@@ -0,0 +1,10 @@
+document.querySelector('.burger-menu').addEventListener('click', () => {
+    document.querySelector('.burger-menu').classList.toggle('opened')
+})
+
+
+// w-4/5 h-0.5 bg-[#373F41] rounded absolute delay-[0.625s] before:content-[''] before:w-full before:h-0.5 before:bg-[#373F41] before:rounded before:absolute before:top-[-0.5rem] before:delay-[0.5s]
+//                 after:content-[''] after:w-full after:h-0.5 after:bg-[#373F41] after:rounded after:absolute after:top-[0.5rem] after:delay-[0.75s]
+
+
+// w-full h-0 rounded absolute bg-red-500 rotate-45 top-[45%] left-0 delay-[0] before:rotate-90 before:content-[''] before:w-full before:h-0 before:bg-red-500 before:rounded before:absolute before:left-0 before:delay-[0.25s]
