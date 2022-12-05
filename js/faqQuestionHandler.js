170e7a902694fcc2b73c5d8c1ebcd3f18313068a
ber4bbit <ber4bbit@gmail.com>
2022-12-06 03:28:00 +0700

    fixed mobile version

--- js/faqQuestionHandler.js
+++ js/faqQuestionHandler.js
@@ -2,7 +2,7 @@ const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach(item => {
     item.addEventListener('click', () => {
-        // item.childNodes[3].classList.toggle('invisible')
+        item.childNodes[3].classList.toggle('hidden')
 
         item.childNodes[3].style.opacity == '0'
             ? item.childNodes[3].style.opacity = '1'
@@ -10,11 +10,6 @@ faqItems.forEach(item => {
                 ? item.childNodes[3].style.opacity = '0'
                 : null
 
-        item.childNodes[3].style.height == '0px'
-            ? item.childNodes[3].style.height = '10rem'
-            : item.childNodes[3].style.height == '10rem'
-                ? item.childNodes[3].style.height = '0px'
-                : null
 
         item.childNodes[1].children[1].innerText == '+'
             ? item.childNodes[1].children[1].innerText = '-'
